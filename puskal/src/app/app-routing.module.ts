2a3d5283bef140c5ed0b96a1a9e3f5895ca9a6c5
Puskal Kafle <puskally@gmail.com>
2019-01-10 22:20:17 +0545

    banner section

--- puskal/src/app/app-routing.module.ts
+++ puskal/src/app/app-routing.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DepartmentListComponent } from './department-list/department-list.component';
-import { DepartmentDetailComponent } from './department-detail/department-detail.component';
-import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { DepartmentOverviewComponent } from './department-overview/department-overview.component';
-import { DepartmentContactComponent } from './department-contact/department-contact.component';
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 
 const routes: Routes = [
-  { path: '', redirectTo: '/departments', pathMatch: 'full' },
-  { path: 'departments', component: DepartmentListComponent },
-  { 
-    path: 'departments/:id', 
-    component: DepartmentDetailComponent,
-    children: [
-      { path: 'overview', component: DepartmentOverviewComponent},
-      { path: 'contact', component: DepartmentContactComponent}
-    ]
-   },
-  { path: 'employees',   component: EmployeeListComponent },
+  // { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'about-me',   component: AboutMeComponent },
   { path: '**',   component: PageNotFoundComponent }
   
 ];
@@ -29,9 +19,8 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [DepartmentListComponent,
-                                  DepartmentDetailComponent, 
-                                  EmployeeListComponent,
+export const routingComponents = [
                                   PageNotFoundComponent,
-                                  DepartmentOverviewComponent,
-                                  DepartmentContactComponent]
+                                  HomeComponent,
+                                  AboutMeComponent
+                             ]
