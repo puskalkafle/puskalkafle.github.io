5ec6bc5db7c77540fade39f4c198033c8d25eb3a
Puskal Kafle <puskally@gmail.com>
2019-01-21 23:29:15 +0545

    work section filter implemented

--- puskal/src/app/home/home.component.ts
+++ puskal/src/app/home/home.component.ts
@@ -1,14 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-// import { WorkListComponent} from '../work-list/work-list.component';
+import { Component, OnInit,Directive, Input, ViewChild } from '@angular/core';
+import { WorkListComponent} from '../work-list/work-list.component';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
 
-  constructor() { }
+export class HomeComponent implements OnInit {
+  @ViewChild(WorkListComponent)
+  private worklistcomponent:WorkListComponent;
+
+  constructor() {
+
+  }
 
   ngOnInit() {
   }
--- puskal/src/app/work-list/work-list.component.ts
+++ puskal/src/app/work-list/work-list.component.ts
@@ -1,33 +1,54 @@
-import { Component, OnInit } from '@angular/core';
-import { WorkService } from './../work.service';
+import {Component, OnInit} from '@angular/core';
+import {WorkService} from './../work.service';
 
 @Component({
-  selector: 'app-work-list',
-  templateUrl: './work-list.component.html',
-  styleUrls: ['./work-list.component.scss']
+    selector: 'app-work-list',
+    templateUrl: './work-list.component.html',
+    styleUrls: ['./work-list.component.scss']
 })
 export class WorkListComponent implements OnInit {
 
-  public works = [];
-  public errorMsg;
-  public limitResult = 2;
-  constructor(private _workService: WorkService) {
+    public works = [];
+    public show_works = [];
+    public errorMsg;
+    public limitResult = 2;
+    public userOption = 'Web Development';
+    public showAll = true;
 
-  }
+    constructor(private _workService: WorkService) {
 
-  ngOnInit() {
-    this._workService.getAllWorks()
-        .subscribe(data => {
+    }
 
-          this.works = data
-            console.log(this.works)
-            },
-            error => this.errorMsg = error);
+    ngOnInit() {
 
-  }
-  loadMore(){
-    this.limitResult = this.limitResult + 1 ;
-    console.log(this.limitResult);
-  }
+        this.setWorkData();
+    }
+
+    setWorkData() {
+        this._workService.getAllWorks()
+            .subscribe(data => {
+                this.works = data;
+                this.show_works = this.works;
+                },
+                error => this.errorMsg = error);
+    }
+
+    loadMore() {
+        this.limitResult = this.limitResult + 1;
+        console.log(this.limitResult);
+    }
+
+    setUserOption(option) {
+        let result;
+        if (option != "all") {
+            result = this.works.filter(i => i.slug == option);
+        } else {
+            // this.setWorkData();
+            result = this.works;
+        }
+        console.log(option);
+     this.show_works = result;
+
+    }
 
 }
