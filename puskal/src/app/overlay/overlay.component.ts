6aed263ea127e478157d98cc1189fd26f4e91a40
Puskal Kafle <puskally@gmail.com>
2019-02-19 21:53:40 +0545

    preview box double sidebar issue fixed

--- puskal/src/app/overlay/overlay.component.ts
+++ puskal/src/app/overlay/overlay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-overlay',
@@ -9,16 +9,21 @@ export class OverlayComponent implements OnInit {
 
   public preview_url = '';
   public preview_hide = true;
-  constructor() { }
+  constructor(private renderer: Renderer2) {
+
+  }
 
   ngOnInit() {
   }
   preview(src){
     this.preview_hide = false;
     this.preview_url = 'assets/'+src;
-}
+    this.renderer.addClass(document.body, 'preview-box');
+
+  }
 hidepreview(){
     this.preview_hide = true;
+    this.renderer.removeClass(document.body, 'preview-box');
 }
 
 }
