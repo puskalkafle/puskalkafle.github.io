777176c3fd5daaa443e3fb2d68804cb76ac78de7
Puskal Kafle <puskal.kafle@diagonal.software>
2021-01-20 23:34:51 +0545

    portfolio page design

--- source/pages/portfolio/index.tsx
+++ source/pages/portfolio/index.tsx
@@ -1,58 +1,118 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Container, Col, Row, setConfiguration } from 'react-grid-system';
-import { faTwitter } from '@fortawesome/free-brands-svg-icons'
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Masonry from 'react-masonry-css'
 
 
 setConfiguration({ containerWidths: [540, 740, 960, 1140, 1140] });
 
 interface Props { }
+const breakpointColumnsObj = {
+  default: 4,
+  1100: 3,
+  700: 2,
+  500: 1
+};
+
+
 
 const Landing: React.FunctionComponent<Props> = () => {
   let d = new Date();
   let currentYear = d.getFullYear();
-
+  let items: { id: number; name: string; }[];
+  let el: any[];
+  items = [
+    { id: 1, name: 'My First Item' },
+    { id: 2, name: 'Another itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother itemAnother item' },
+    { id: 3, name: 'Third Item' },
+    { id: 4, name: 'Here is the Fourth' },
+    { id: 5, name: 'High Five' }
+  ];
+  el = items.map(function (item) {
+    return <div key={item.id}>{item.name}</div>
+  });
   return (
     <Main>
-
-      <Container>
-        <ContentWrapper>
-          <div className="top">
-            <div className="row">
-              <a href="/"><img src="logo.svg" /></a>
-              <Box className="box-top-right-border"></Box>
-            </div>
+      <ContentWrapper>
+        <div className="top">
+          <div className="row">
+            <a href="/"><img src="logo.svg" /></a>
+            <Box className="box-top-right-border"></Box>
           </div>
-          <div className="bottom">
-            <div className="row">
-              <Box className="box-bottom-left-border"></Box>
-              <div className="social">
-                <FontAwesomeIcon icon={faTwitter} />
-                <FontAwesomeIcon icon={faLinkedin} />
-                / <span>{currentYear}</span>
-                </div>
-            </div>
-          </div>
-        </ContentWrapper>
-      </Container>
-      <div className="mid">
+        </div>
+        <div className="mid">
         <div className="main-text">
           <div>PORTFOLIO</div>
           <div id="filters">
+            <a href="#">All</a>/
             <a href="#">Design</a>/
             <a href="#">Development</a>/
             <a href="#">Art</a>/
             <a href="#">Photography</a>
-            </div>
+          </div>
 
         </div>
       </div>
+      <Container>
+        <Masonry
+          breakpointCols={breakpointColumnsObj}
+          className="my-masonry-grid"
+          columnClassName="my-masonry-grid_column"
+        >
+          {el}
+        </Masonry>
+      </Container>
+        <div className="bottom">
+          <div className="row">
+            <Box className="box-bottom-left-border"></Box>
+            <div className="social">
+              <FontAwesomeIcon icon={faTwitter} />
+              <FontAwesomeIcon icon={faLinkedin} />
+                / <span>{currentYear}</span>
+            </div>
+          </div>
+        </div>
+      </ContentWrapper>
+
     </Main>
   );
 };
 const Main = styled.main`
+.my-masonry-grid {
+  display: -webkit-box; /* Not needed if autoprefixing */
+  display: -ms-flexbox; /* Not needed if autoprefixing */
+  display: flex;
+  margin-left: -30px; /* gutter size offset */
+  width: auto;
+}
+.my-masonry-grid_column {
+  padding-left: 30px; /* gutter size */
+  background-clip: padding-box;
+}
+@media (max-width: 800px) {
+  .my-masonry-grid {
+    margin-left: -15px; /* gutter size offset */
+  }
+  .my-masonry-grid_column {
+    padding-left: 15px; /* gutter size offset */
+  }
+  .my-masonry-grid_column > div {
+    margin-bottom: 15px; /* space between items */
+  }
+}
+ 
+/* Style your items */
+.my-masonry-grid_column > div { /* change div to reference your elements you put in <Masonry> */
+  background: grey;
+  margin-bottom: 30px;
+}
+
+
+
+
 .mid{
   justify-content: center;
   font-size:150px;
@@ -100,6 +160,7 @@ const ContentWrapper = styled.div`
 display:flex;
 flex-direction:column;
 justify-content: space-aroung;
+padding:0 ${({ theme }) => theme.spacing.xxxxl};
 &>div{
   height:50vh;
   max-height:50vh;
