9dc5dd7d25c10eec1d1b083962b11691a6332b6e
Puskal Kafle <puskal.kafle@diagonal.software>
2021-12-24 20:30:30 +0545

    title changed

--- source/pages/index.tsx
+++ source/pages/index.tsx
@@ -8,12 +8,12 @@ const Main: React.FunctionComponent<Props> = () => {
   return (
     <>
       <Head>
-        <title>Puskal - Designer and Developer</title>
+        <title>Puskal - UX Engineer</title>
         <link rel="icon" href="/favicon.ico" />
         {/* <!-- Search Engine --> */}
 <meta name="google-site-verification" content="g5XrM5CffAuoNakjzOepDxTQshDzTQvaHzQVh4nw5dw" />
 <meta name="title" content="Puskal Kafle - UX/UI Designer and Frontend Developer"/>
-<meta name="description" content="Puskal Kafle is Napel based frontend developer and UI/UX designer. Visit this website to see his work."/>
+<meta name="description" content="Puskal Kafle is Napel based UX Engineer. Visit website to see his work."/>
 <meta name="keywords" content="UI mockups, prototypes, wireframes, style guides, standards, graphic elements, design suggestions, reusable code, collaborate, usability, bug-free, communication skills, detail-oriented, JavaScript, HTML5, CSS3, RESTful APIs, GUI, Color Theory, Typography, Adobe XD, Adobe Photoshop, Adobe Illustrator, Puskal Kafle, Kathmandu, Nepal, designer, developer, graphic designer, software developer, web developer, web application, freelancer, javascript, d3js, data visualization, jquery, angular, git, best desiger, top designer, emerging technologies, json, npm, digital painting, vector art, branding, style guideline"/>
 <meta name="robots" content="index, follow"/>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
@@ -21,7 +21,7 @@ const Main: React.FunctionComponent<Props> = () => {
 <meta name="author" content="Puskal Kafle"/>
 {/* <!-- Twitter --> */}
 <meta name="twitter:card" content="summary"/>
-<meta name="twitter:title" content="Puskal Kafle - Web Designer and Developer"/>
+<meta name="twitter:title" content="Puskal Kafle - UX Engineer"/>
 <meta name="twitter:description" content="Frontend developer and designer from Kathmandu, Nepal"/>
 <meta name="twitter:site" content="@puskal"/>
 <meta name="twitter:creator" content="@puskal"/>
