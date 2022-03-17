7fef2023b9c497fe6029643b4d6687eb3cb1455a
Puskal Kafle <puskal.kafle@diagonal.software>
2022-03-17 20:55:43 +0545

    detail page design

--- source/pages/portfolio/[slug].tsx
+++ source/pages/portfolio/[slug].tsx
@@ -1,20 +1,72 @@
-import { useRouter } from 'next/router'
-import Header from '../header'
-import ContentWrapper from '../components/contentWrapper'
+import { useRouter } from 'next/router';
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import Header from '../header';
+import Data from '../../data/data.json';
 
-const PortfolioDetail = () => {
-  const router = useRouter()
-  const slug = router.query.slug || []
-console.log(slug)
+// https://www.azzerad.com/
+function PortfolioDetail() {
+  const router = useRouter();
+  const mainData = Data.data;
+  let portfolioItem: any;
+  const [data, setFilterData] = useState([]);
+
+  useEffect(() => {
+    const slugId = router.query.slug;
+
+    if (!!data && slugId) {
+      portfolioItem = Object.values(mainData).filter((d, i) =>
+        i === parseInt(slugId))[0];
+      if (portfolioItem) {
+        setFilterData(portfolioItem);
+      }
+    }
+  });
+
   return (
     <>
       <Header />
-      <ContentWrapper>
-      <h1>In Progress</h1>
-      <h4>Portfolio Detail Page</h4>
-      </ContentWrapper>
+        <ContentContainer>
+          <div className="left">
+            <em>{data.type}</em>
+            <h2 dangerouslySetInnerHTML={{ __html: data.title }} />
+            <p>{data.desc}</p>
+          </div>
+          <div className="right">
+            <img
+              src={`/assets/img/work/${data.img}`}
+            />
+          </div>
+        </ContentContainer>
     </>
-  )
+  );
 }
 
-export default PortfolioDetail
+const ContentContainer = styled.div`
+    display:flex;
+    width: 1024px;
+    margin: 0 auto;
+    img{
+      max-width:100%;
+    }
+    .left,.right{
+      width:100%;
+    }
+    .left{
+      em{
+        font-size: 18px;
+        margin-bottom: 5px;
+        display: inline-block;
+      }
+      h2{
+        margin-bottom: 15px;
+      }
+      p{
+        font-size: 28px;
+        font-weight: 300;
+        padding-right: 1rem;
+      }
+    }
+`;
+
+export default PortfolioDetail;
